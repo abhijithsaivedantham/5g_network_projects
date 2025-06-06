d70a9c22f3a66d5c553425a73c7a906f51faa314
abhijithsaivedantham <abhijithsai963@gmail.com>
2025-06-06 09:19:26 +0530

    deleted

--- pdcp_project3/channel_simulator.py
+++
@@ -1,8 +0,0 @@
-import random
-from config import TAMPERING_RATE
-
-class ChannelSimulator:
-    def transmit(self, pdu):
-        if random.random() < TAMPERING_RATE:
-            pdu.is_corrupted = True
-        return pdu
